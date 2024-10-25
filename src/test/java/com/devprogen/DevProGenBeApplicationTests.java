d602b3500f81aa39eb4d222aee1c3d149ee0b54b
ImMrHak <hakkoumohamed23@gmail.com>
2024-10-25 21:56:16 +0100

    Setup project configuration and initial files
    - Added project configuration files
    
    - Created application-dev.properties with database, Hibernate, mail, JWT, and OAuth2 settings.
    
    - Added @SpringBootApplication(exclude = { SecurityAutoConfiguration.class }) to DevProGenBeApplication.java to disable Spring Security's auto-configuration, allowing for a custom security setup.

---
+++ src/test/java/com/devprogen/DevProGenBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devprogen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevProGenBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
